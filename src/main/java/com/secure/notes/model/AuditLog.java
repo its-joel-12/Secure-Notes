b382246e6c2e85b65a6cbd17fbb96bc6941d3d48
Joel Oliveira <joel80851@gmail.com>
2025-06-03 12:21:23 +0530

    Implementing Audit Service Layer

---
+++ src/main/java/com/secure/notes/model/AuditLog.java
@@ -0,0 +1,23 @@
+package com.secure.notes.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class AuditLog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String action;
+    private String username;
+    private Long noteId;
+    private String noteContent;
+    private LocalDateTime timestamp;
+}
