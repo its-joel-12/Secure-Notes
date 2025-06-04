45f41e668bb66303e6ec5fafc62fdcab5c1af705
Joel Oliveira <joel80851@gmail.com>
2025-06-04 16:35:59 +0530

    Validating the Password Reset Token

--- src/main/java/com/secure/notes/model/PasswordResetToken.java
+++ src/main/java/com/secure/notes/model/PasswordResetToken.java
@@ -2,6 +2,7 @@ package com.secure.notes.model;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.Instant;
@@ -9,6 +10,7 @@ import java.time.Instant;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class PasswordResetToken {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
